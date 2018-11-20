2aa3cb52e39a89f71bae06e59b23cfe61436f17d
jterry <jordankterry@outlook.com>
2018-11-20 15:13:49 -0700

    updates

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -29,9 +29,10 @@ class Home extends Component {
     
     <img id="hero-img1" src="../images/hero.png" />
     <img id="hero-img2" src="../images/hero-mobile.png" />
+    <img id="hero-img3" src="../images/hero-mobile2.png" />
     
 
-     <div className="card-panel z-depth-0 center-align" id="slogan">
+     {/* <div className="card-panel z-depth-0 center-align" id="slogan">
       <span className="white-text" id="slogan-text1">
         The employee experience platform <br></br>
       </span>
@@ -43,7 +44,28 @@ class Home extends Component {
       <a href="" className="btn-flat center-align" id="getStarted">
         Get Started
       </a>
-    </div>
+    </div> */}
+
+
+    <div className="col s12 m4">
+            <div className="top-card">
+            <div className="card-content center">
+              <div className="icon-block">
+               
+                <h5 className="center" id="slogan-text1" style={{fontSize: '40px', color: 'white'}}>The Employee Experience Platform</h5>
+                <p className="light center" id="slogan-text2" style={{fontSize: "38px", color: 'white', lineHeight: '0px'}}>
+                  That makes a difference
+                </p>
+                <br></br>
+                <a href="" className="btn-flat" id="getStarted">
+                  Get Started
+                </a>
+                </div>
+                </div>
+              </div>
+            </div>
+
+    
    
     </div>
     <Nav />
